77c534ccf81033ab1655453ccf9993fd8a93ba38
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-07-19 20:24:28 -0400

    Ajustes para pruebas y readme

--- buhocenter-frontend/src/tests/eva4DSW.spec.ts
+++ buhocenter-frontend/src/tests/eva4DSW.spec.ts
@@ -1,11 +1,12 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
-describe('Prueba de creacion de ofertas', () => {
+describe('Prueba de creación de ofertas', () => {
     it('Inicio de sesión de administrador y creación de ofertas', async () => {
+        let check = true;
         try {
             let driver = await new Builder().forBrowser('chrome').build();
             await driver.get('http://localhost:8080/home');
-            await driver.manage().window().fullscreen();
+            await driver.manage().window().maximize();
             await driver.findElement(By.id('btnSignIn')).click();
             await driver.findElement(By.id('inputEmail')).sendKeys('admin@gmail.com');
             await driver.findElement(By.id('inputPassword')).sendKeys('123456');
@@ -24,15 +25,18 @@ describe('Prueba de creacion de ofertas', () => {
             await driver.wait(until.elementLocated(By.id('snackbar')), 10000);
             await driver.findElement(By.id('seeOffer')).click();
         } catch (e) {
-            console.log(e);
+            check = false;
+            console.log('Error en test: ', e.message);
         }
+        expect(check).toBeTruthy();
     }, 30000);
 
     it('Inicio de sesión de administrador y fallo en creación de ofertas', async () => {
+        let check = true;
         try {
             let driver = await new Builder().forBrowser('chrome').build();
             await driver.get('http://localhost:8080/home');
-            await driver.manage().window().fullscreen();
+            await driver.manage().window().maximize();
             await driver.findElement(By.id('btnSignIn')).click();
             await driver.findElement(By.id('inputEmail')).sendKeys('admin@gmail.com');
             await driver.findElement(By.id('inputPassword')).sendKeys('123456');
@@ -48,21 +52,26 @@ describe('Prueba de creacion de ofertas', () => {
             await driver.findElement(By.id('inputName')).sendKeys('Oferta especial');
             await driver.findElement(By.id('inputDescription')).sendKeys('');
         } catch (e) {
-            console.log(e);
+            check = false;
+            console.log('Error en test: ', e.message);
         }
+        expect(check).toBeTruthy();
     }, 30000);
 
     it('Fallo en inicio de sesión de administrador y fallo en creación de ofertas', async () => {
+        let check = true;
         try {
             let driver = await new Builder().forBrowser('chrome').build();
             await driver.get('http://localhost:8080/home');
-            await driver.manage().window().fullscreen();
+            await driver.manage().window().maximize();
             await driver.findElement(By.id('btnSignIn')).click();
             await driver.findElement(By.id('inputEmail')).sendKeys('admingmail.com');
             await driver.findElement(By.id('inputPassword')).sendKeys('123456');
             await driver.findElement(By.id('signIn')).click();
         } catch (e) {
-            console.log(e);
+            check = false;
+            console.log('Error en test: ', e.message);
         }
+        expect(check).toBeTruthy();
     }, 30000);
 });
